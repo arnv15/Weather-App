e4815f88e5ec594f7352d4dff7c1abe2486ba3cf
Arnav Gupta <guptaji1015@gmail.com>
2024-03-19 22:26:49 -0700

    small changes, edited some things so it looks better

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def get_weather_data(location):
     session = requests.Session()
     session.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
     html = session.get(url)
-
     soup = bs(html.text, "html.parser")
     name = soup.find("div", attrs={"id": "wob_loc"}).text
     time = soup.find("div", attrs={"id": "wob_dts"}).text
-    weather = soup.find("div", attrs={"id": "wob_dc"}).text
-    temp = soup.find("div", attrs={"id": "wob_tm"}).text
+    weather = soup.find("span", attrs={"id": "wob_dc"}).text
+    temp = soup.find("span", attrs={"id": "wob_tm"}).text
     return name, time, weather, temp
+
 
 sg.theme("reddit")
 image_col = sg.Column([[sg.Image(key="-IMAGE-", background_color="#FFFFFF")]])
@@ -34,10 +34,11 @@ while True:
     if event == sg.WIN_CLOSED:
         break
     if event == "Enter":
-        get_weather_data(values["-INPUT-"])
-        window["-LOCATION-"].update("test", visible=True)
-        window["-TIME-"].update("test", visible=True)
-        window["-TEMP-"].update("test", visible=True)
-        window["-IMAGE-"].update("C:/Users/gupta/OneDrive/Pictures/Screenshots/sun.png")
+        name, time, weather, temp = get_weather_data(values["-INPUT-"])
+        temp = str(temp) + "°F"
+        window["-LOCATION-"].update(name, visible=True)
+        window["-TIME-"].update(time, visible=True)
+        window["-TEMP-"].update(temp, visible=True)
+        window["-IMAGE-"].update("C:/Users/gupta/OneDrive/Pictures/Screenshots/sunny.png")
 
 window.close()
