54282c9472e5b34bb90f2d93e4fbd6ac439eb7e7
Arnav Gupta <guptaji1015@gmail.com>
2024-03-19 21:04:20 -0700

    added the things for the basic layout and changed little design aspects

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import PySimpleGUI as sg
+sg.theme("reddit")
 
 image_col = sg.Column([[sg.Image(key="-IMAGE-", background_color="#FFFFFF")]])
 info_col = sg.Column([
-    [sg.Text("", key="-LOCATION-", font="Calibri 30", background_color="#FF0000", pad=0, visible=False)],
+    [sg.Text("", key="-LOCATION-", font="Calibri 30", background_color="#FF0000", text_color="#FFFFFF", pad=0, visible=False)],
     [sg.Text("", key="-TIME-", font="Calibri 16", background_color="#000000", text_color="#FFFFFF", pad=0, visible=False)],
     [sg.Text("", key="-TEMP-", font="Calibri 16", pad=(0,10), background_color="#FFFFFF", text_color="#000000", justification="center", visible=False)]
 ])
 layout = [
-    [sg.Input(expand_x=True, key="-INPUT-"), sg.Button("Enter")],
+    [sg.Input(expand_x=True, key="-INPUT-"), sg.Button("Enter", button_color="#000000", border_width=0)],
     [image_col, info_col]
 ]
 
@@ -17,5 +18,10 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
+    if event == "Enter":
+        window["-LOCATION-"].update("test", visible=True)
+        window["-TIME-"].update("test", visible=True)
+        window["-TEMP-"].update("test", visible=True)
+        window["-IMAGE-"].update("C:/Users/gupta/OneDrive/Pictures/Screenshots/sun.png")
 
 window.close()
