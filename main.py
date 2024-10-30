d9c6c1f33243cd87ac19401c1e101ef07604a38f
Arnav Gupta <guptaji1015@gmail.com>
2024-10-29 22:07:46 -0700

    shifted images directly into this file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup as bs
 import requests
 
 def get_weather_data(location):
-    url = f"https://www.google.com/search?q=weather+{location.replace(" ", "")}"
+    url = f'https://www.google.com/search?q=weather+{location.replace(" ", "")}'
     session = requests.Session()
     session.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
     html = session.get(url)
@@ -35,51 +35,51 @@ def update_imag():
     window["-IMAGE-"].update(visible=True)
     # sun
     if weather in ("Sun", "Sunny", "Clear"):
-        window["-IMAGE-"].update("C:/Users/gupta/OneDrive/Pictures/Screenshots/sunny.png")
+        window["-IMAGE-"].update("images/sunny.png")
 
     # periodic clouds
     if weather in ("Clear with periodic clouds", "Mostly Sunny"):
-        window["-IMAGE-"].update("C:/Users/gupta/OneDrive/Pictures/Screenshots/sunny_s_cloudy.png")
+        window["-IMAGE-"].update("images/sunny_s_cloudy.png")
 
     # partly cloudy
     if weather in ("Partly Sunny", "Partly cloudy", "Cloudy periodically clear"):
-        window["-IMAGE-"].update("C:/Users/gupta/OneDrive/Pictures/Screenshots/partly-cloudy.png")
+        window["-IMAGE-"].update("images/partly-cloudy.png")
 
     # cloudy
     if weather in ("Mostly cloudy", "Cloudy", "Overcast"):
-        window["-IMAGE-"].update("C:/Users/gupta/OneDrive/Pictures/Screenshots/cloudy.png")
+        window["-IMAGE-"].update("images/cloudy.png")
 
     # light rain
     if weather in ("Light rain", "Chance of Rain"):
-        window["-IMAGE-"].update("C:/Users/gupta/OneDrive/Pictures/Screenshots/rain_light.png")
+        window["-IMAGE-"].update("images/rain_light.png")
 
     # rain
     if weather in ("Rain and Snow", "Hail"):
-        window["-IMAGE-"].update("C:/Users/gupta/OneDrive/Pictures/Screenshots/snow_s_rain.png")
+        window["-IMAGE-"].update("images/snow_s_rain.png")
 
     # heavy rain
     if weather in ("Rain", "Showers", "Scattered Showers"):
-        window["-IMAGE-"].update("C:/Users/gupta/Onedrive/Pictures/Screenshots/rain.png")
+        window["-IMAGE-"].update("images/rain.png")
 
     # thunder
     if weather in ( "Chance of Storms", "Storm", "Thunderstorm", "Chance of Tstorm", "Thunderstorms and rain"):
-        window["-IMAGE-"].update("C:/Users/gupta/OneDrive/Pictures/Screenshots/thunderstorms.png")
+        window["-IMAGE-"].update("images/thunderstorms.png")
     
     # Light thunderstorms
     if weather in ("Scattered Thunderstorms", "Light thunderstorms and rain", "Isolated thunderstorms", "Cloudy with periodic rain"):
-        window["-IMAGE-"].update("C:/Users/gupta/Onedrive/Pictures/Screenshots/rain_s_cloudy.png")
+        window["-IMAGE-"].update("images/rain_s_cloudy.png")
 
     # foggy
     if weather in ("Mist", "Dust", "Fog", "Smoke", "Haze", "Flurries"):
-        window["-IMAGE-"].update("C:/Users/gupta/OneDrive/Pictures/Screenshots/cloudy.png")
+        window["-IMAGE-"].update("images/cloudy.png")
 
     # snow
     if weather in ("Freezing Drizzle", "Chance of Snow", "Sleet", "Snow", "Icy", "Snow Showers"):
-        window["-IMAGE-"].update("C:/Users/gupta/OneDrive/Pictures/Screenshots/snow.png")
+        window["-IMAGE-"].update("images/snow.png")
 
     # windy
     if weather in ("Windy"):
-        window["-IMAGE-"].update("C:/Users/gupta/OneDrive/Pictures/Screenshots/windy.png")
+        window["-IMAGE-"].update("images/windy.png")
     
 def in_case_of_error():
     text = f"No results for \"{cache[0]}\""
